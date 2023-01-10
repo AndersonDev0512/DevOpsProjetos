0ab3b8b774cf1d81b8c2b8a8ab6e697e82824900
AndersonDev0512 <anderson.erdeval@gmail.com>
2023-01-10 00:51:37 -0400

    commit com bugs, preciso resolver

--- ansible-lab/control-node/provision.sh
+++ ansible-lab/control-node/provision.sh
@@ -1,9 +1,9 @@
 #/bin/sh 
-sudo yum -y install epel-release
-echo "inicio da instalacao do ansible"
-sudo yum -y install ansible 
-cat <<EOT >> /etc/hosts
-192.168.56.12 control-node
-192.168.56.13 app01
-192.168.56.14 db01
-EOT 
+#sudo yum -y install epel-release
+#echo "inicio da instalacao do ansible"
+#sudo yum -y install ansible 
+#cat <<EOT >> /etc/hosts
+#192.168.56.12 control-node
+#192.168.56.13 app01
+#192.168.56.14 db01
+#EOT 
