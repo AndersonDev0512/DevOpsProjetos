a15923f5e0f9c4a53b05dbad443493d468405cd0
AndersonDev0512 <anderson.erdeval@gmail.com>
2023-01-20 20:13:15 -0400

    commit com todos os laboratorios

--- ansible-lab/control-node/provision.sh
+++ ansible-lab/control-node/provision.sh
@@ -1,11 +1,9 @@
-#/bin/sh 
+#/bin/sh
 sudo yum -y install epel-release
 echo "inicio da instalacao do ansible"
-sudo yum -y install ansible 
-sudo cat <<EOT >> /etc/hosts
+sudo yum -y install ansible
+cat <<EOT >> /etc/hosts
 192.168.56.12 control-node
 192.168.56.13 app01
 192.168.56.14 db01
-EOT 
-sudo yum install -y nfs-utils 
-echo "inicio da instalação do nfs"
+EOT
