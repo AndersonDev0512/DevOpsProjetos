930f5d9a4ef1150427014a9240a99a2437f6321f
AndersonDev0512 <anderson.erdeval@gmail.com>
2023-02-09 22:03:23 -0400

    iniciado o lab-docker e feito o deploy de uma aplicação node com redis

--- ansible-lab/control-node/provision.sh
+++ ansible-lab/control-node/provision.sh
@@ -2,7 +2,7 @@
 sudo yum -y install epel-release
 echo "inicio da instalacao do ansible"
 sudo yum -y install ansible
-cat <<EOT >> /etc/hosts
+cat <<EOT >> /etc/'hosts'
 192.168.56.12 control-node
 192.168.56.13 app01
 192.168.56.14 db01
