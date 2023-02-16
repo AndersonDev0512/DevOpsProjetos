a4a15549fa7de47cc92e1182b0d3302a9fc61f7e
AndersonDev0512 <anderson.erdeval@gmail.com>
2023-02-16 08:35:23 -0400

    finalizado o laboratorio de sonarqube

--- sonarqube-lab/provision.sh
+++ sonarqube-lab/provision.sh
@@ -1,24 +1,35 @@
 /usr/bin/bash
 useradd sonar
-yum  install wget unzip java-11-openjdk-devel -y 
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.0.65466.zip
-unzip sonarqube-9.9.0.65466.zip -d /opt/
-mv /opt/sonarqube-9.9.0.65466 /opt/sonarqube
-sudo chown -R sonar:sonar /opt/sonarqube
-sudo touch /etc/systemd/system/sonar.service
-echo > /etc/systemd/system/sonar.service
-sudo cat <<EOT >> /etc/systemd/system/sonar.service
-[Unit]
-Description = Sonarqube Service 
-after=syslog.target network.target
-[Service]
-Type=forking
-ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
-ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
-User=sonar
-Group=sonar
-Restart=always
-[Install]
-WantedBy=multi-user.target
-EOT
-service sonar start
+#yum  install wget unzip java-11-openjdk-devel -y 
+#wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.1.0.47736.zip
+#unzip sonarqube-9.1.0.47736.zip -d /opt/
+#sudo mkdir /opt/sonarqube
+#sudo mv /opt/sonarqube-9.1.0.47736 /opt/sonarqube
+#sudo chown -R sonar:sonar /opt/sonarqube
+#sudo touch /etc/systemd/system/sonar.service
+#echo > /etc/systemd/system/sonar.service
+#sudo cat <<EOT >> /etc/systemd/system/sonar.service
+#[Unit]
+#Description = Sonarqube Service 
+#after=syslog.target network.target
+#[Service]
+#Type=forking
+#ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+#ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+#User=sonar  
+#Group=sonar
+#Restart=always
+#[Install]
+#WantedBy=multi-user.target
+#EOT
+#service sonar start
+
+#instalar sonar scanner
+
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
+sudo unzip sonar-scanner-cli-4.6.2.2472-linux.zip -d /opt/
+sudo mv /opt/sonar-scanner-cli-4.6.2.2472 /opt/sonar-scanner
+sudo chown -R sonar:sonar /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' |sudo tee -a /etc/profile
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install nodejs -y
